e7acaf284786cb5a2fd92726599e3089c13909fb
najorcruzcruz <najorcruzcruz@gmail.com>
2018-06-19 17:59:29 +0100

    [IR-2208] Adds drag start and drag end events

---
+++ diagram-builder/src/main/java/org/vaadin/diagrambuilder/listener/TaskDragEndListener.java
@@ -0,0 +1,12 @@
+package org.vaadin.diagrambuilder.listener;
+
+import org.vaadin.diagrambuilder.dto.NodeDto;
+
+/*
+ * Copyright (c) 2012-2018 Continuum Security SLNE.  All rights reserved
+ */
+public interface TaskDragEndListener {
+
+    public void onDragEnd(NodeDto nodeDto);
+
+}
